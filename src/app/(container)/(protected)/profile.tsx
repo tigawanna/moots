b22a6df5f6402e0be00cb799bb9ea641ad0a2e2d
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-15 23:38:51 +0300

    checkpoint: abput to add ath

---
+++ src/app/(container)/(protected)/profile.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+import { Text,Surface } from 'react-native-paper';
+ 
+export function profile(){
+return (
+<Surface style={{ ...styles.container }}>
+    <Text variant='titleLarge'>profile</Text>
+</Surface>
+);
+}
+const styles = StyleSheet.create({
+container:{
+  flex:1,
+  height:'100%',
+   width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+})
