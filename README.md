698940f84abddccc8ea7ba1c2a35fe2978bb3d96
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-16 23:53:46 +0300

    refactor: undoing appwrite protected routes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-## Moots
+# Moots
+
 
 Moots is a local-first, social movie curation platform that enables users to create and share curated movie lists, discover content through community-driven recommendations, and engage with fellow movie enthusiasts. The app combines LiveStore's offline-first reactive data management with external movie APIs to create a seamless, social movie discovery experience that works both online and offline.
--- src/app/(container)/_layout.tsx
+++ src/app/(container)/_layout.tsx
@@ -1,23 +1,21 @@
-import { LoadingFallback } from "@/components/screens/state-screens/LoadingFallback";
-import { viewerQueryOptions } from "@/lib/tanstack/auth/auth";
-import { useQuery } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 
 // this grouped routes  (contaner) layout exists because tanstcak query provider is defined in the root layout making it hard to useQuery to check for logged i user in that layout
 
 export default function ContainerLayout() {
-  const { data, isPending } = useQuery(viewerQueryOptions());
+  // const { data, isPending } = useQuery(viewerQueryOptions());
 
-  if (isPending) {
-    return <LoadingFallback />;
-  }
-  const isAuthenticated = !!data?.$id;
+  // if (isPending) {
+  //   return <LoadingFallback />;
+  // }
+  // const isAuthenticated = !!data?.$id;
   return (
     <Stack>
-      <Stack.Protected guard={isAuthenticated}>
-        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-      </Stack.Protected>
-      <Stack.Screen name="signin" options={{ headerShown: true }} />
+      {/* <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack.Protected> */}
+      {/* <Stack.Screen name="signin" options={{ headerShown: true }} /> */}
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
