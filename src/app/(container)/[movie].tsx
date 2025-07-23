c30ed8824294fd5f91aeefdf93531c0c70af1c72
tigawanna <denniskinuthiawaweru@gmail.com>
2025-07-23 14:38:35 +0300

    feat: Add MovieDetails and ShowDetails components with local search parameters

---
+++ src/app/(container)/[movie].tsx
@@ -0,0 +1,21 @@
+import { useLocalSearchParams } from "expo-router";
+import { StyleSheet } from "react-native";
+import { Text, Surface } from "react-native-paper";
+
+export function MovieDetails() {
+  const { movie } = useLocalSearchParams() as { movie: string };
+  return (
+    <Surface style={{ ...styles.container }}>
+      <Text variant="titleLarge">Movie {movie}</Text>
+    </Surface>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ src/app/(container)/[show].tsx
@@ -0,0 +1,21 @@
+import { useLocalSearchParams } from "expo-router";
+import { StyleSheet } from "react-native";
+import { Text, Surface } from "react-native-paper";
+
+export function ShowDetails() {
+  const { show } = useLocalSearchParams() as { show: string };
+  return (
+    <Surface style={{ ...styles.container }}>
+      <Text variant="titleLarge">Show {show}</Text>
+    </Surface>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
